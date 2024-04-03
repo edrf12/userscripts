5bfe8682e8a2d3d651aaa935c0f03c9f72a8105e
edrf12 <me@edrf.tech>
2024-04-02 23:13:14 -0300

    Add scripts

---
+++ PTable Annoyance Remover.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        PTable Annoyance Remover
+// @description Removes the don't like ads banner and the bottom bar
+// @match       *://ptable.com/*
+// @version      1.0
+// @author       edrf12
+// @updateURL    https://github.com/edrf12/userscripts/raw/main/PTable-Annoyance-Remover.user.js
+// @downloadURL  https://github.com/edrf12/userscripts/raw/main/PTable-Annoyance-Remover.user.js
+// ==/UserScript==
+
+let elements = document.querySelectorAll(
+	"#Notice, body > header > label:nth-child(7), #Wide, body > header > label:nth-child(9), #TopbarToggle, body > header > label:nth-child(11), #SidebarToggle, body > header > label:nth-child(13), #DarkButton, body > footer"
+);
+
+elements.forEach((elmnt) => {
+	elmnt.remove();
+});
